2f642398fb57de909464d9604fd8650a69eba496
nazar.popovych <nazar.popovych1918@gmail.com>
2017-05-17 22:42:49 +0300

    bn-01 dao for tenderdetail

---
+++ src/main/java/com/blackneptune/dao/daoImpl/TenderDetailDaoImpl.java
@@ -0,0 +1,9 @@
+package com.blackneptune.dao.daoImpl;
+
+import com.blackneptune.dao.TenderDetailDao;
+
+/**
+ * Created by Admin on 5/17/2017.
+ */
+public class TenderDetailDaoImpl implements TenderDetailDao {
+}
